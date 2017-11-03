9f477b78b1682ddca523b9f523c29c40a61e8e22
Ulfasaar <weyers.ryan@gmail.com>
2017-11-03 20:22:31 +1300

    Started Omega code ported GPIO to haxe

---
+++ Main.hx
@@ -0,0 +1,20 @@
+// import php.Lib;
+
+class Main {
+  static public function main():Void {
+    Omega.SetPin(44, "HIGH");
+    // trace("Hello World");
+  }
+}
+
+// import js.Browser;
+// class Main {
+//     static function main() {
+//         var button = Browser.document.createButtonElement();
+//         button.textContent = "Click me!";
+//         button.onclick = function(event) {
+//           Browser.alert("Haxe is great");
+//         }
+//         Browser.document.body.appendChild(button);
+//     }
+// }
---
+++ Omega.hx
@@ -0,0 +1,53 @@
+  /**
+  * @Author: Ryan Weyers
+  * @Date: 3/11/2017
+  * @Description: A nifty haxe wrapper for Onion omega 2 GPIO pins based on the OmegaPHP code written by Chris McCaslin on 4/8/2016
+  * @found at: https://gist.githubusercontent.com/Immortal-/a18f58ac5c21ba27921b7626b5a8b06e/raw/df8e70665523c2a06b503954d10943560d5c189f/OmegaPHP.php
+  */
+
+import sys.io.Process;
+class Omega{
+
+    static function execute(command: String): String{
+        return new Process(command).stdout.readAll().toString().split(': ')[1];
+    }
+
+    /**
+    * @MethodName: SetPin
+    * @Arg $pin The pin to be 'set'
+    * @Arg $value The value you want the pin to be set to ie. 1,0,HIGH,LOW
+    */
+    static public function SetPin(pin:Int, user_value: String)
+    {
+        var value: Int = 0;
+
+        if(user_value.toLowerCase() == "high")
+        {
+            value = 1;
+        }else if (user_value.toLowerCase() == "high") //I wanted the HIGH, LOW constant I have with Arduino.
+        {
+            value = 0;
+        }
+
+        return execute("fast-gpio set " + pin + " " + value + " 2>&1");
+        
+    }
+    /**
+    * @MethodName: ReadPin
+    * @Arg $pin The pin to be 'read'
+    */
+    static public function ReadPin(pin: Int)
+    {
+        return execute("fast-gpio read " + pin + " 2>&1");
+    }
+    /**
+    * @MethodName: SetPWM
+    * @Arg $pin The pin to be 'set'
+    * @Arg $hZ The value you want for frequency set in hertz.
+    * @Arg $dutyCyclePercent The Duty Cycle Percent
+    */
+    static public function SetPWM(pin, hZ, dutyCyclePercent)
+    {
+        return execute("fast-gpio pwm " + pin + " " + hZ + " " + dutyCyclePercent + " 2>&1");
+    }
+}
