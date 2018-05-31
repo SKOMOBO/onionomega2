d72f4eb73c1a7922336e2e71308dc9ada9730fb8
Ryan Weyers <weyers.ryan@gmail.com>
2018-05-31 22:46:55 +1200

    Removed haxe files

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php include "omega.php";
 
-echo "hello world";
-$a1 = int(OmegaPHP.ReadI2C("0x28", "0x28"));
-$a2 = int(OmegaPHP.ReadI2C("0x28", "0x28"));
-$a3 = int(OmegaPHP.ReadI2C("0x28", "0x28"));
-$a4 = int(OmegaPHP.ReadI2C("0x28", "0x28"));
+$a1 = (int)OmegaPHP.ReadI2C("0x28", "0x28");
+$a2 = (int)OmegaPHP.ReadI2C("0x28", "0x28");
+$a3 = (int)OmegaPHP.ReadI2C("0x28", "0x28");
+$a4 = (int)OmegaPHP.ReadI2C("0x28", "0x28");
 
 echo($a1 + $a2 + $a3 + $a4);
 ?>
